f0724c8d05e0bf0c2275713d1685f057982aab18
A-tak <hoge+git@a-tak.com>
2018-10-03 21:08:01 +0900

    DOMへのアクセス

--- vue/05Vue.js基本機能/main.js
+++ vue/05Vue.js基本機能/main.js
@@ -1,13 +1,6 @@
 new Vue({
     el: '#app',
-    data: {
-        list:[]
-    },
-    created: function() {
-        axios.get('list.json').then(function(response) {
-            this.list = response.data
-        }.bind(this)).catch(function(e) {
-            console.error(e)
-        })
+    mounted: function() {
+        console.log(this.$refs.hello)
     }
 })
