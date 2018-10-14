39793053a374c1d4143d6a7b52ccb7513641f53b
A-tak <hoge+git@a-tak.com>
2018-10-14 14:44:36 +0900

    コンポーネントに突入

--- vue/04算出プロパティ/main.js
+++ vue/04算出プロパティ/main.js
@@ -1,3 +1,11 @@
 var app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        width : 80
+    },
+    computed: {
+        halfWidth: function() {
+            return this.width /2
+        }
+    }
 })
---
+++ vue/05コンポーネント/main.js
@@ -0,0 +1,6 @@
+Vue.component('my-component',{
+    template: '<p>MyComponent</p>'
+})
+var app = new Vue({
+    el: '#app'
+})
