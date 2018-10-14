2283fdfeb0ffcdc0f6922707d822db98dd4432c4
A-tak <hoge+git@a-tak.com>
2018-10-14 13:31:54 +0900

    コンソールにエラーで照るけど動いてるな。イベントハンドラ

--- vue/05Vue.js基本機能/main.js
+++ vue/05Vue.js基本機能/main.js
@@ -1,8 +1,8 @@
 var app = new Vue({
     el: '#app',
-    data: {
-        message : {
-            value: "Heello"
+    methods: {
+        handleClick: function() {
+            alert('クリックしたな!')
         }
     }
 })
