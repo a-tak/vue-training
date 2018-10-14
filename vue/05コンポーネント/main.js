c28bc1e2f78028a623a3164647dae80a09cd831c
A-tak <hoge+git@a-tak.com>
2018-10-14 16:15:36 +0900

    親から子へ

--- vue/05コンポーネント/main.js
+++ vue/05コンポーネント/main.js
@@ -1,9 +1,14 @@
-var myComponent = {
-    template: '<p>MyComponent</p>'
-}
+Vue.component('comp-child', {
+    template: '<li>{{ name }} HP. {{ hp }}</li>',
+    props: ['name', 'hp']
+})
 var app = new Vue({
     el: '#app',
-    components: {
-        'my-component': myComponent
+    data : {
+        list: [
+            { id: 1, name: 'すらりん', hp: 100 },
+            { id: 2, name: 'ごぶらん', hp: 200 },
+            { id: 3, name: 'どらがん', hp: 500 }
+        ]
     }
 })
