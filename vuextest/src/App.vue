208659f7ed148194fec2f551396f8ab342857bf6
A-tak <hoge+git@a-tak.com>
2018-10-28 20:11:58 +0900

    store初級

--- vuextest/src/App.vue
+++ vuextest/src/App.vue
@@ -8,6 +8,30 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+// 補完機能に任せて書いたが、ここだとこういう書き方になるのがよくわからん。スコープだろうけど。
+// https://012-jp.vuejs.org/guide/components.html
+export default Vue.extend({
+  created() : void {
+    //ストアから値取得
+    console.log(this.$store.state.count);
+    //ストアの値アップ ミューテーションで定義された操作でしか値を操作できない
+    this.$store.commit('increment');
+    //ステートに直接アクセスするよりgetterに統一した方がいいらしい
+    console.log('getter count = ' + this.$store.getters.count);
+    //値を変更してやる!
+    this.$store.commit('setcount',99);
+    console.log('getter count = ' + this.$store.getters.count);
+    //actionで複雑なことをしてやる!
+    //dispatchはほんとう戻り値にpromise貰えるので終了したかを検知できる!
+    this.$store.dispatch('setName', "A-tak");
+    console.log('getter username = ' + this.$store.getters.username);
+
+  }
+})
+</script>
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- vuextest/src/store.ts
+++ vuextest/src/store.ts
@@ -5,12 +5,36 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    count: 0,
+    username : ""
   },
   mutations: {
-
+    //デフォでstateやpayload(commitの時の引数)が渡されるらしい
+    increment(state, payload) {
+        state.count++
+    },
+    setcount(state, payload) {
+        state.count = payload
+    },
+    setName(state, payload) {
+        state.username = payload
+    }
   },
   actions: {
-
+    //名前の頭にアスタリスクを着けて返すaction
+    setName({ commit }, payload) {
+        let newname : string = payload;
+        newname = '*' + newname;
+        commit('setName',newname)
+    }
+  },
+  getters: {
+      //このstateは引数?後ろのgettersは何?
+      count(state, getters, rootState, rootGetter) {
+          return state.count;
+      },
+      username(state) {
+          return state.username;
+      }
   }
 })
