2189333a00e9a3da9dc0b7aeab26fd76d8d06598
A-tak <hoge+git@a-tak.com>
2018-11-01 22:38:50 +0900

    tasksに空でも定義入れないとエラーになる

--- taskclear-proto/src/store.ts
+++ taskclear-proto/src/store.ts
@@ -5,7 +5,9 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    tasks:[]
+    tasks:[
+      {id: '', title: ''},
+    ]
   },
   getters: {
     tasks(state) { return state.tasks }
