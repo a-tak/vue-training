a61572872c57413c220134db9af22372ab9fed6b
A-tak <hoge+git@a-tak.com>
2018-11-03 22:35:02 +0900

    時間は一旦0時0分に

--- taskclear-proto/src/components/NewTask.vue
+++ taskclear-proto/src/components/NewTask.vue
@@ -26,8 +26,11 @@ export default class NewTask extends Vue {
   addTask() : void {
     if (this.inputvalue.trim()!="") {
       //ここがタスクの追加部分
+      const d: Date = this.$store.getters.targetDate;
+      // 一旦時間は0:00でセット。セクションを取り入れるときはここの時間をセクションの時間に変更する
+      const date = new Date(d.getFullYear(),d.getMonth(),d.getDate(),0,0,0,0);
       this.$store.commit("addTask",
-         {id: uuid(), date: this.$store.getters.targetDate,  title: this.inputvalue});
+         {id: uuid(), date: date,  title: this.inputvalue});
 
       this.inputvalue = "";
 
