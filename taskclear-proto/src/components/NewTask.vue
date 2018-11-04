f45c2eb2903ac659407c89d561c545034b866e3c
A-tak <hoge+git@a-tak.com>
2018-11-04 14:21:24 +0900

    日本語確定でタスク追加が誤動作しない対処を入れた https://qiita.com/h-orito/items/6d99e2256074132d7867

--- taskclear-proto/src/components/NewTask.vue
+++ taskclear-proto/src/components/NewTask.vue
@@ -3,7 +3,7 @@
       <v-card>
         <v-layout row wrap fill-height align-center justify-center>
             <v-flex class="ma-2">
-                <v-text-field placeholder="新しいタスクを追加" outline v-model="inputvalue" clearable v-on:keyup.enter="addTask"></v-text-field>
+                <v-text-field placeholder="新しいタスクを追加" outline v-model="inputvalue" clearable @keyup.enter="addTask" @keypress="setCanSubmit"></v-text-field>
             </v-flex>
             <v-flex>
                 <v-btn @click="addTask">追加</v-btn>
@@ -22,21 +22,27 @@ import uuid from 'uuid';
 export default class NewTask extends Vue {
 
   inputvalue: string = "";
+  canSubmit: boolean = false;
+
+  setCanSubmit() {
+    this.canSubmit = true;
+  }
 
   addTask() : void {
-    if (this.inputvalue.trim()!="") {
-      //ここがタスクの追加部分
-      const d: Date = this.$store.getters.targetDate;
-      // 一旦時間は0:00でセット。セクションを取り入れるときはここの時間をセクションの時間に変更する
-      const date = new Date(d.getFullYear(),d.getMonth(),d.getDate(),0,0,0,0);
-      this.$store.commit("addTask",
-         {id: uuid(), date: date,  title: this.inputvalue});
+    if (this.inputvalue.trim()=="" || this.canSubmit == false) return;
 
-      this.inputvalue = "";
+    //ここがタスクの追加部分
+    const d: Date = this.$store.getters.targetDate;
+    // 一旦時間は0:00でセット。セクションを取り入れるときはここの時間をセクションの時間に変更する
+    const date = new Date(d.getFullYear(),d.getMonth(),d.getDate(),0,0,0,0);
+    this.$store.commit("addTask",
+        {id: uuid(), date: date,  title: this.inputvalue});
 
-      firebase.firestore().collection("tasks").doc(this.$store.getters.user.uid).set({ tasks: this.$store.getters.tasks });
+    firebase.firestore().collection("tasks").doc(this.$store.getters.user.uid).set({ tasks: this.$store.getters.tasks });
 
-    }
+    this.inputvalue = "";
+    this.canSubmit = false;
+
   }
 
 
