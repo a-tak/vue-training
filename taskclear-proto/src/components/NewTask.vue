b3645aed56546d2e281a5273a2d8076fd3e54ff5
A-tak <hoge+git@a-tak.com>
2018-11-10 22:39:26 +0900

    頭にアンダーバー着けたプロパティはVueでは認識されないのでまた命名規則を変えた

--- taskclear-proto/src/components/NewTask.vue
+++ taskclear-proto/src/components/NewTask.vue
@@ -3,7 +3,7 @@
       <v-card>
         <v-layout row fill-height align-center justify-center>
             <v-flex mt-4 ml-2>
-                <v-text-field placeholder="新しいタスクを追加" single-line outline v-model="inputvalue" clearable hint="記載したらEnterか追加ボタン" @keyup.enter="addTask" @keypress="setCanSubmit"></v-text-field>
+                <v-text-field placeholder="新しいタスクを追加" single-line outline v-model="inputvalue_" clearable hint="記載したらEnterか追加ボタン" @keyup.enter="addTask" @keypress="setCanSubmit"></v-text-field>
             </v-flex>
             <v-flex ma-2>
                 <v-btn @click="addTask">追加</v-btn>
@@ -25,27 +25,28 @@ import Task from '../lib/Task';
 @Component
 export default class NewTask extends Vue {
 
-  inputvalue: string = "";
-  canSubmit: boolean = false;
+  //なぜかinputvalueはアンダーバーつけるとエラーになる
+  inputvalue_: string = "";
+  canSubmit_: boolean = false;
 
   setCanSubmit() {
-    this.canSubmit = true;
+    this.canSubmit_ = true;
   }
 
   addTask() : void {
-    if (this.inputvalue.trim()=="" || this.canSubmit == false) return;
+    if (this.inputvalue_.trim()=="" || this.canSubmit_ == false) return;
 
     //ここがタスクの追加部分
     const d: Date = this.$store.getters.targetDate;
     // 一旦時間は0:00でセット。セクションを取り入れるときはここの時間をセクションの時間に変更する
     const date = new Date(d.getFullYear(),d.getMonth(),d.getDate(),0,0,0,0);
-    let task:Task = new Task(date,this.inputvalue);
+    let task:Task = new Task(date,this.inputvalue_);
     this.$store.commit("addTask",task);
 
     fb.saveTasks(this.$store.getters.user.uid, this.$store.getters.targetDate,this.$store.getters.taskCtrl);
 
-    this.inputvalue = "";
-    this.canSubmit = false;
+    this.inputvalue_ = "";
+    this.canSubmit_ = false;
 
   }
 
