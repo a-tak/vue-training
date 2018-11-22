171f200ddbd21b7379436b1d99fd5bf446aa5eff
A-tak <hoge+git@a-tak.com>
2018-11-22 22:18:20 +0900

    closed #29 ローカル開発環境、ステージング環境、本番環境でプロジェクトをわけられるようにした

--- taskclear-proto/src/main.ts
+++ taskclear-proto/src/main.ts
@@ -9,12 +9,12 @@ import firebase from 'firebase'
 Vue.config.productionTip = false
 
 var config = {
-  apiKey: "AIzaSyA94oovqbFPrXWWfeoUwZbJX65JfSiYh7c",
-  authDomain: "a-tak-test.firebaseapp.com",
-  databaseURL: "https://a-tak-test.firebaseio.com",
-  projectId: "a-tak-test",
-  storageBucket: "a-tak-test.appspot.com",
-  messagingSenderId: "190278648588"
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
