e97e4efeeff846f75e7be902c10b0d39fa46fc1c
A-tak <hoge+git@a-tak.com>
2018-11-24 17:21:15 +0900

    デザイン修正

--- taskclear-proto/src/components/RepeatEdit.vue
+++ taskclear-proto/src/components/RepeatEdit.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="repeat-edit">
         <v-card class="ma-2">
+            <v-card-title>
+                リピート設定
+            </v-card-title>
             <v-layout v-bind="layoutAttributes" fill-height align-center justify-center row>
                 <v-flex ml-3>
                     <v-layout column fill-height align-center justify-center>
