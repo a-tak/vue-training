91d440b3a4ff1d02bbfa3c33def9914cc0474196
A-tak <hoge+git@a-tak.com>
2018-11-25 10:28:13 +0900

    スマホの時のタイトル表示を調整

--- taskclear-proto/src/components/Login.vue
+++ taskclear-proto/src/components/Login.vue
@@ -2,7 +2,7 @@
     <div id="home">
         <v-container>
             <v-layout align-center justify-center column fill-height>
-                <v-flex display-4 ma-5>
+                <v-flex v-bind="logoSize" ma-5>
                     Taskclear
                 </v-flex>
                 <v-flex v-if="!loading_">
@@ -19,7 +19,7 @@
                         <v-card-title primary-title>
                             ご注意
                         </v-card-title>
-                        <v-card-text class="text-md-left">
+                        <v-card-text class="text-xs-left">
                             <span>データを消す場合がありますのであくまでお試しの利用に限ってください</span><br>
                             <span>メールや登録したデータは管理者は見えてしまいますので、ほんとあくまでお試しで</span>
                         </v-card-text>
@@ -51,6 +51,18 @@ export default class Login extends Vue {
 
     created() {
         console.log(`created loading state = ${this.loading_}`);
+    }
+
+    get logoSize() : {} {
+        //画面サイズによって入力ボックスを横に並べるか縦に並べるか切り替える
+        switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return {"display-3": true};
+            case 'sm': return {"display-3": true};
+            case 'md': return {"display-4": true};
+            case 'lg': return {"display-4": true};
+            case 'xl': return {"display-4": true};
+            default  : return {"display-4": true};
+        }
     }
 
 }
